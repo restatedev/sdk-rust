44a4255d71a3ff0a2fee60d6cf806fb77c9b5920
Kevin Webb <kpwebb@gmail.com>
2025-02-19 08:58:11 -0500

    cleanup / lint + fmt fixes

--- test-env/src/lib.rs
+++ test-env/src/lib.rs
@@ -1,18 +1,29 @@
-use tracing::{error, info, warn};
-use testcontainers::{core::{IntoContainerPort, WaitFor}, runners::AsyncRunner, ContainerAsync, ContainerRequest, GenericImage, ImageExt};
-use serde::{Serialize, Deserialize};
-use restate_sdk::{errors::HandlerError, prelude::{Endpoint, HttpServer}};
-use tokio::{io::AsyncBufReadExt, net::TcpListener, task::{self, JoinHandle}};
+use restate_sdk::{
+    errors::HandlerError,
+    prelude::{Endpoint, HttpServer},
+};
+use serde::{Deserialize, Serialize};
 use std::time::Duration;
+use testcontainers::{
+    core::{IntoContainerPort, WaitFor},
+    runners::AsyncRunner,
+    ContainerAsync, ContainerRequest, GenericImage, ImageExt,
+};
+use tokio::{
+    io::AsyncBufReadExt,
+    net::TcpListener,
+    task::{self, JoinHandle},
+};
+use tracing::{error, info, warn};
 
 // addapted from from restate-admin-rest-model crate version 1.1.6
 #[derive(Serialize, Deserialize, Debug)]
 pub struct RegisterDeploymentRequestHttp {
     uri: String,
-    additional_headers:Option<Vec<(String, String)>>,
+    additional_headers: Option<Vec<(String, String)>>,
     use_http_11: bool,
     force: bool,
-    dry_run: bool
+    dry_run: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -25,33 +36,38 @@ pub struct RegisterDeploymentRequestLambda {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct VersionResponse {
-    version:String,
-    min_admin_api_version:u32,
-    max_admin_api_version:u32
+    version: String,
+    min_admin_api_version: u32,
+    max_admin_api_version: u32,
 }
 
 pub struct TestContainerBuilder {
-    container_name:String,
-    container_tag:String,
-    logging:bool
+    container_name: String,
+    container_tag: String,
+    logging: bool,
+}
+
+impl Default for TestContainerBuilder {
+    fn default() -> Self {
+        TestContainerBuilder {
+            container_name: "docker.io/restatedev/restate".to_string(),
+            container_tag: "latest".to_string(),
+            logging: false,
+        }
+    }
 }
 
 impl TestContainerBuilder {
-
-    pub fn new() -> TestContainerBuilder {
-
-        TestContainerBuilder { container_name: "docker.io/restatedev/restate".to_string(),
-                                container_tag: "latest".to_string(),
-                                logging: false }
-
-    }
-
     pub fn with_container_logging(mut self) -> TestContainerBuilder {
         self.logging = true;
         self
     }
 
-    pub fn with_container(mut self, container_name:String, container_tag:String) -> TestContainerBuilder {
+    pub fn with_container(
+        mut self,
+        container_name: String,
+        container_tag: String,
+    ) -> TestContainerBuilder {
         self.container_name = container_name;
         self.container_tag = container_tag;
 
@@ -59,44 +75,44 @@ impl TestContainerBuilder {
     }
 
     pub fn build(self) -> TestContainer {
-        TestContainer { builder: self,
-                        container: None,
-                        endpoint_task: None,
-                        endpoint_server_ip: None,
-                        endpoint_server_port: None,
-                        endpoint_server_url: None,
-                        ingress_url: None,
-                        stdout_logging: None,
-                        stderr_logging: None }
+        TestContainer {
+            builder: self,
+            container: None,
+            endpoint_task: None,
+            endpoint_server_ip: None,
+            endpoint_server_port: None,
+            endpoint_server_url: None,
+            ingress_url: None,
+            stdout_logging: None,
+            stderr_logging: None,
+        }
     }
 }
 
 pub struct TestContainer {
-    builder:TestContainerBuilder,
-    container:Option<ContainerAsync<GenericImage>>,
-    endpoint_task:Option<JoinHandle<()>>,
-    endpoint_server_ip:Option<String>,
-    endpoint_server_port:Option<u16>,
-    endpoint_server_url:Option<String>,
-    ingress_url:Option<String>,
-    stdout_logging:Option<JoinHandle<()>>,
-    stderr_logging:Option<JoinHandle<()>>,
+    builder: TestContainerBuilder,
+    container: Option<ContainerAsync<GenericImage>>,
+    endpoint_task: Option<JoinHandle<()>>,
+    endpoint_server_ip: Option<String>,
+    endpoint_server_port: Option<u16>,
+    endpoint_server_url: Option<String>,
+    ingress_url: Option<String>,
+    stdout_logging: Option<JoinHandle<()>>,
+    stderr_logging: Option<JoinHandle<()>>,
+}
+
+impl Default for TestContainer {
+    fn default() -> Self {
+        TestContainerBuilder::default().build()
+    }
 }
 
 impl TestContainer {
-
-    pub fn new() -> TestContainer {
-        TestContainerBuilder::new().build()
-    }
-
     pub fn builder() -> TestContainerBuilder {
-
-        TestContainerBuilder::new()
-
+        TestContainerBuilder::default()
     }
 
-    pub async fn start(mut self, endpoint:Endpoint) -> Result<TestContainer, anyhow::Error> {
-
+    pub async fn start(mut self, endpoint: Endpoint) -> Result<TestContainer, anyhow::Error> {
         self.serve_endpoint(endpoint).await?;
         self.start_container().await?;
         let registered = self.register_endpoint().await;
@@ -107,46 +123,60 @@ impl TestContainer {
         Ok(self)
     }
 
-    async fn serve_endpoint(&mut self, endpoint:Endpoint) -> Result<(), anyhow::Error> {
-
+    async fn serve_endpoint(&mut self, endpoint: Endpoint) -> Result<(), anyhow::Error> {
         info!("Starting endpoint server...");
 
         // use port 0 to allow tokio to assign unused port number
-        let host_address = format!("127.0.0.1:0");
-        let listener = TcpListener::bind(host_address).await.expect("listener can bind");
-        self.endpoint_server_ip =  Some(listener.local_addr().unwrap().ip().to_string());
+        let host_address = "127.0.0.1:0".to_string();
+        let listener = TcpListener::bind(host_address)
+            .await
+            .expect("listener can bind");
+        self.endpoint_server_ip = Some(listener.local_addr().unwrap().ip().to_string());
         self.endpoint_server_port = Some(listener.local_addr().unwrap().port());
-        self.endpoint_server_url = Some(format!("http://{}:{}", self.endpoint_server_ip.as_ref().unwrap(), self.endpoint_server_port.as_ref().unwrap()));
+        self.endpoint_server_url = Some(format!(
+            "http://{}:{}",
+            self.endpoint_server_ip.as_ref().unwrap(),
+            self.endpoint_server_port.as_ref().unwrap()
+        ));
 
         // boot restate server
         self.endpoint_task = Some(tokio::spawn(async move {
-            HttpServer::new(endpoint)
-            //.serve(listener).await;
-                .serve(listener).await;
+            HttpServer::new(endpoint).serve(listener).await;
         }));
 
         let client = reqwest::Client::builder().http2_prior_knowledge().build()?;
 
         // wait for server to respond
-        while let Err(_) = client.get(format!("{}/discover", self.endpoint_server_url.as_ref().unwrap()))
-                                .header("accept", "application/vnd.restate.endpointmanifest.v1+json")
-                                .send().await {
-            tokio::time::sleep(Duration::from_secs(1)).await;
+        let mut retries = 0;
+        while client
+            .get(format!(
+                "{}/discover",
+                self.endpoint_server_url.as_ref().unwrap()
+            ))
+            .header("accept", "application/vnd.restate.endpointmanifest.v1+json")
+            .send()
+            .await
+            .is_err()
+        {
+            tokio::time::sleep(Duration::from_millis(100)).await;
 
             warn!("retrying endpoint server");
+
+            retries += 1;
+            if retries > 10 {
+                return Err(anyhow::anyhow!("endpoint server failed to start"));
+            }
         }
 
-        client.get(format!("{}/discover", self.endpoint_server_url.as_ref().unwrap()))
-            .header("accept", "application/vnd.restate.endpointmanifest.v1+json")
-            .send().await?;
-
-        info!("endpoint server: {}", self.endpoint_server_url.as_ref().unwrap());
+        info!(
+            "endpoint server: {}",
+            self.endpoint_server_url.as_ref().unwrap()
+        );
 
         Ok(())
     }
 
     async fn start_container(&mut self) -> Result<(), anyhow::Error> {
-
         let image = GenericImage::new(&self.builder.container_name, &self.builder.container_tag)
             .with_exposed_port(9070.tcp())
             .with_exposed_port(8080.tcp())
@@ -154,13 +184,17 @@ impl TestContainer {
 
         // have to expose entire host network because testcontainer-rs doesn't implement selective SSH port forward from host
         // see https://github.com/testcontainers/testcontainers-rs/issues/535
-        self.container = Some(ContainerRequest::from(image)
-            .with_host("host.docker.internal" , testcontainers::core::Host::HostGateway)
-            .start()
-            .await?);
+        self.container = Some(
+            ContainerRequest::from(image)
+                .with_host(
+                    "host.docker.internal",
+                    testcontainers::core::Host::HostGateway,
+                )
+                .start()
+                .await?,
+        );
 
         if self.builder.logging {
-
             let container_stdout = self.container.as_ref().unwrap().stdout(true);
             let mut stdout_lines = container_stdout.lines();
 
@@ -206,7 +240,6 @@ impl TestContainer {
             self.stderr_logging = Some(stderr_logging);
         }
 
-
         let host = self.container.as_ref().unwrap().get_host().await?;
         let ports = self.container.as_ref().unwrap().ports().await?;
 
@@ -222,8 +255,10 @@ impl TestContainer {
     }
 
     async fn register_endpoint(&mut self) -> Result<(), HandlerError> {
-
-        info!("registering endpoint server: {}", self.endpoint_server_url.as_ref().unwrap());
+        info!(
+            "registering endpoint server: {}",
+            self.endpoint_server_url.as_ref().unwrap()
+        );
 
         let host = self.container.as_ref().unwrap().get_host().await?;
         let ports = self.container.as_ref().unwrap().ports().await?;
@@ -232,26 +267,32 @@ impl TestContainer {
 
         let client = reqwest::Client::builder().http2_prior_knowledge().build()?;
 
-        let deployment_uri:String = format!("http://host.docker.internal:{}/", self.endpoint_server_port.unwrap());
+        let deployment_uri: String = format!(
+            "http://host.docker.internal:{}/",
+            self.endpoint_server_port.unwrap()
+        );
         let deployment_payload = RegisterDeploymentRequestHttp {
-                                    uri:deployment_uri,
-                                    additional_headers:None,
-                                    use_http_11: false,
-                                    force: false,
-                                    dry_run: false }; //, additional_headers: (), use_http_11: (), force: (), dry_run: () }
+            uri: deployment_uri,
+            additional_headers: None,
+            use_http_11: false,
+            force: false,
+            dry_run: false,
+        }; //, additional_headers: (), use_http_11: (), force: (), dry_run: () }
 
         let register_admin_url = format!("http://{}:{}/deployments", host, admin_port);
 
-        client.post(register_admin_url)
+        client
+            .post(register_admin_url)
             .json(&deployment_payload)
-            .send().await?;
+            .send()
+            .await?;
 
         let ingress_port = ports.map_to_host_port_ipv4(8080.tcp()).unwrap();
         self.ingress_url = Some(format!("http://{}:{}", host, ingress_port));
 
         info!("ingress url: {}", self.ingress_url.as_ref().unwrap());
 
-        return Ok(());
+        Ok(())
     }
 
     pub fn ingress_url(&self) -> String {
@@ -261,7 +302,6 @@ impl TestContainer {
 
 impl Drop for TestContainer {
     fn drop(&mut self) {
-
         // testcontainers-rs already implements stop/rm on drop]
         // https://docs.rs/testcontainers/latest/testcontainers/
 
--- test-env/tests/test_container.rs
+++ test-env/tests/test_container.rs
@@ -23,7 +23,6 @@ trait MyWorkflow {
     async fn my_shared_handler(input: String) -> HandlerResult<String>;
 }
 
-
 struct MyServiceImpl;
 
 impl MyService for MyServiceImpl {
@@ -35,38 +34,45 @@ impl MyService for MyServiceImpl {
 
 #[tokio::test]
 async fn test_container() {
-
     tracing_subscriber::fmt::fmt()
-                    .with_max_level(tracing::Level::INFO) // Set the maximum log level
-                    .init();
+        .with_max_level(tracing::Level::INFO) // Set the maximum log level
+        .init();
 
-    let endpoint = Endpoint::builder()
-                    .bind(MyServiceImpl.serve())
-                    .build();
+    let endpoint = Endpoint::builder().bind(MyServiceImpl.serve()).build();
 
     // simple test container intialization with default configuration
     // let mut test_container = TestContainer::new().start(endpoint).await.unwrap();
 
     // custom test container initialization with builder
     let test_container = TestContainer::builder()
-                                // optional passthrough logging from the resstate server testcontainer
-                                // prints container logs to tracing::info level
-                                .with_container_logging()
-                                .with_container("docker.io/restatedev/restate".to_string(),  "latest".to_string())
-                                .build()
-                                .start(endpoint).await.unwrap();
+        // optional passthrough logging from the resstate server testcontainer
+        // prints container logs to tracing::info level
+        .with_container_logging()
+        .with_container(
+            "docker.io/restatedev/restate".to_string(),
+            "latest".to_string(),
+        )
+        .build()
+        .start(endpoint)
+        .await
+        .unwrap();
 
     let ingress_url = test_container.ingress_url();
 
     // call container ingress url for /MyService/my_handler
-    let resposne = reqwest::Client::new().post(format!("{}/MyService/my_handler", ingress_url))
-                        .header("Accept", "application/json")
-                        .header("Content-Type", "*/*")
-                        .header("idempotency-key", "abc")
-                        .send().await.unwrap();
+    let response = reqwest::Client::new()
+        .post(format!("{}/MyService/my_handler", ingress_url))
+        .header("Accept", "application/json")
+        .header("Content-Type", "*/*")
+        .header("idempotency-key", "abc")
+        .send()
+        .await
+        .unwrap();
 
-    assert_eq!(resposne.status(), StatusCode::OK);
+    assert_eq!(response.status(), StatusCode::OK);
 
-    info!("/MyService/my_handler response: {:?}", resposne.text().await.unwrap());
-
+    info!(
+        "/MyService/my_handler response: {:?}",
+        response.text().await.unwrap()
+    );
 }
