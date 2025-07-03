905d9669bf4a4affdbe5b4753e29f46f0654ac88
BigFish2086 <a.ibrahim8686@gmail.com>
2025-07-03 20:28:36 +0300

    add test-services testcases to test the new impl-syntax #43
    - updated Cargo.toml to have mutliple binaries, one that used
      the old macro trait-syntax, and another one for the new macro
      struct-impl syntax, so we can test them later, so now
      `cargo build -p test-services` would generate 2 binaries
      trait-test-services & impl-test-services
    
    - updated Dockerfile:
      - to cache the building process through the new mount syntax as well
        as building in multiple stages
      - to have a --build-arg which is BIN that deteremins which macro
        syntax to test when running the restate-sdk-test-suite.jar

---
+++ test-services/src/impl_block/counter.rs
@@ -0,0 +1,59 @@
+use restate_sdk::prelude::*;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use tracing::info;
+
+#[derive(Serialize, Deserialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct CounterUpdateResponse {
+    old_value: u64,
+    new_value: u64,
+}
+
+pub(crate) struct Counter;
+
+const COUNT: &str = "counter";
+
+#[restate_sdk::object(vis = "pub(crate)", name = "Counter")]
+impl Counter {
+    #[handler(shared, name = "get")]
+    async fn get(&self, ctx: SharedObjectContext<'_>) -> HandlerResult<u64> {
+        Ok(ctx.get::<u64>(COUNT).await?.unwrap_or(0))
+    }
+
+    #[handler(name = "add")]
+    async fn add(
+        &self,
+        ctx: ObjectContext<'_>,
+        val: u64,
+    ) -> HandlerResult<Json<CounterUpdateResponse>> {
+        let current = ctx.get::<u64>(COUNT).await?.unwrap_or(0);
+        let new = current + val;
+        ctx.set(COUNT, new);
+
+        info!("Old count {}, new count {}", current, new);
+
+        Ok(CounterUpdateResponse {
+            old_value: current,
+            new_value: new,
+        }
+        .into())
+    }
+
+    #[handler(name = "reset")]
+    async fn reset(&self, ctx: ObjectContext<'_>) -> HandlerResult<()> {
+        ctx.clear(COUNT);
+        Ok(())
+    }
+
+    #[handler(name = "addThenFail")]
+    async fn add_then_fail(&self, ctx: ObjectContext<'_>, val: u64) -> HandlerResult<()> {
+        let current = ctx.get::<u64>(COUNT).await?.unwrap_or(0);
+        let new = current + val;
+        ctx.set(COUNT, new);
+
+        info!("Old count {}, new count {}", current, new);
+
+        Err(TerminalError::new(ctx.key()).into())
+    }
+}
---
+++ test-services/src/mod.rs
@@ -0,0 +1,2 @@
+pub mod impl_block;
+pub mod trait_block;
